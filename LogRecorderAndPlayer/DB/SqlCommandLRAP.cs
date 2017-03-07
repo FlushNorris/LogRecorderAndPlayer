46b565049cd3c079c285bcac167d252120a5332a
Nikolaj Strauss <nst@schantz.com>
2017-03-07 14:38:12 +0100

    SqlCommandLRAP + SqlDataReaderLRAP

---
+++ LogRecorderAndPlayer/DB/SqlCommandLRAP.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogRecorderAndPlayer
+{
+    public class SqlCommandLRAP : IDisposable
+    {
+        public SqlCommand Cmd { get; private set; }
+
+        public SqlCommandLRAP(string sql, SqlConnection conn)
+        {
+            Cmd = new SqlCommand(sql, conn);
+        }
+
+        public SqlParameterCollection Parameters
+        {
+            get { return Cmd.Parameters; }
+        }
+
+        public SqlParameter CreateParameter()
+        {
+            return Cmd.CreateParameter();
+        }
+
+        public SqlDataReaderLRAP ExecuteReader()
+        {
+            return new SqlDataReaderLRAP(Cmd.ExecuteReader());
+        }
+
+        public SqlDataReaderLRAP ExecuteReader(CommandBehavior behavior)
+        {
+            return new SqlDataReaderLRAP(Cmd.ExecuteReader(behavior));
+        }
+
+        public int ExecuteNonQuery()
+        {
+            return Cmd.ExecuteNonQuery();
+        }
+
+        public object ExecuteScalar()
+        {
+            return Cmd.ExecuteScalar();
+        }
+
+        public void Dispose()
+        {
+            Cmd.Dispose();
+        }
+    }
+}
---
+++ LogRecorderAndPlayer/DB/SqlDataReaderLRAP.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogRecorderAndPlayer
+{
+    public class SqlDataReaderLRAP
+    {
+        public SqlDataReader Reader { get; private set; }
+
+        public object this[string name]
+        {
+            get
+            {
+                return Reader[name];
+            }
+        }
+
+        public object this[int i]
+        {
+            get
+            {
+                return Reader[i];
+            }
+        }
+
+        public SqlDataReaderLRAP(SqlDataReader reader)
+        {
+            Reader = reader;
+        }
+
+        public bool Read()
+        {
+            return Reader.Read();
+        }
+
+        public void Close()
+        {
+            Reader.Close();
+        }
+
+        public void Dispose()
+        {
+            Reader.Dispose();
+        }
+
+        public bool IsDBNull(int i)
+        {
+            return Reader.IsDBNull(i);
+        }
+
+        public int GetInt32(int i)
+        {
+            return Reader.GetInt32(i);
+        }
+
+        public DateTime GetDateTime(int i)
+        {
+            return Reader.GetDateTime(i);
+        }
+
+        public bool HasRows => Reader.HasRows;
+    }
+}
