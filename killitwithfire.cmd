3418c01cbdb5640383bb5f82e79bbd696b69538c
Nikolaj Strauss <nst@schantz.com>
2017-10-30 08:56:40 +0100

    Cmds

---
+++ killitwithfire.cmd
@@ -0,0 +1,7 @@
+copy c:\LogRecorderAndPlayer\LogRecorderAndPlayer\bin\Debug\LogRecorderAndPlayer.dll c:\advicedotnet\Applications\Advice.Web.PensionsPlan\bin\
+copy c:\LogRecorderAndPlayer\LogRecorderAndPlayer\bin\Debug\LogRecorderAndPlayer.pdb c:\advicedotnet\Applications\Advice.Web.PensionsPlan\bin\
+
+copy c:\LogRecorderAndPlayer\LogRecorderAndPlayer\bin\Debug\LogRecorderAndPlayer.dll c:\LogRecorderAndPlayer\LogThisWebApplication\LogThisWebApplication\bin\
+copy c:\LogRecorderAndPlayer\LogRecorderAndPlayer\bin\Debug\LogRecorderAndPlayer.pdb c:\LogRecorderAndPlayer\LogThisWebApplication\LogThisWebApplication\bin\
+
+
